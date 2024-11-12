ac698815f04c5cf32cb312a72e54689b08a7fd3b
Gabriel Sousa <gabriellopes4769@gmail.com>
2024-11-12 14:49:32 -0300

    feat: Enviando estado de coleta

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 # Dicionário para armazenar os dados dos dispositivos
 # Estrutura: { "device1": {"contador": valor, "temperatura": valor, "timestamp": datetime} }
 devices_data = {}
+devices_state = {}
 
 # Página inicial com a tabela de dispositivos
 @app.route('/')
@@ -51,6 +52,27 @@ def get_device_data(device_id):
         return jsonify(devices_data[device_id])
     else:
         return jsonify({"error": "Dispositivo não encontrado"}), 404
+
+@app.route('/send_state', methods=['POST'])
+def send_state():
+    # Recebe os dados do estado do dispositivo
+    data = request.get_json()
+    device_id = data.get('deviceID')
+    state = devices_state.get(device_id, False)
+
+    devices_state[device_id] = not state
+
+    # Aqui você pode processar o estado e enviar um comando para o dispositivo
+    # Simulando que o dispositivo recebeu o estado
+    print(f"Enviando o estado {state} para o dispositivo {device_id}")
+
+    # Para o exemplo, vamos simplesmente retornar um JSON com o dispositivo e o estado
+    response = {
+        'deviceID': device_id,
+        'state': state,
+        'status': 'comando enviado com sucesso'
+    }
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host='192.168.115.253')
